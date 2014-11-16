c77833fcc791a67030a22c5e88ab4b03fcaa64fc
zhkuskov <me.kuskov@gmail.com>
2014-11-16 17:49:49 +0300

    Update multi_dropdown.js

--- js/multi_dropdown.js
+++ js/multi_dropdown.js
@@ -3,10 +3,8 @@ $('ul.dropdown-menu [data-toggle=dropdown]').on('click', function(event) {
     event.preventDefault(); 
     // Avoid having the menu to close when clicking
     event.stopPropagation(); 
-    // If a menu is already open we close it
-    //$('ul.dropdown-menu [data-toggle=dropdown]').parent().removeClass('open');
-    // opening the one you clicked on
-    $(this).parent().addClass('open');
+    // opening the one you clicked on.  If a menu is already open we close it
+    $(this).parent().toggleClass('open');
 
     var menu = $(this).parent().find("ul");
     var menupos = menu.offset();
